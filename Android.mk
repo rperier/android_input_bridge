2ce0bbf2b1f8bfef8800f887af19134cfb50195d
Vishal Bhoj <vishal.bhoj@linaro.org>
2012-07-04 14:45:41 +0000

    compile the host client in Android build system
    Signed-off-by: Romain Perier <bambi@ubuntu.com>

--- Android.mk
+++ Android.mk
@@ -8,3 +8,14 @@ LOCAL_SRC_FILES:=    \
 LOCAL_MODULE := aibd
 LOCAL_CFLAGS += -fno-strict-aliasing -DAPPNAME=\"aidb\"
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:=    \
+    common.c         \
+    aib.c
+LOCAL_MODULE := aib
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_IS_HOST_MODULE := true
+LOCAL_CFLAGS += -fno-strict-aliasing -DAPPNAME=\"aib\"
+include $(BUILD_HOST_EXECUTABLE)
